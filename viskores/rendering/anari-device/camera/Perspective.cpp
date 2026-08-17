b3056d10d1773338110b94627cb464554f1abf00
DeVasconcelos <malheiros.susanne@gmail.com>
2026-08-17 15:06:09 +0200

    make m_aspect optional

--- viskores/rendering/anari-device/camera/Perspective.cpp
+++ viskores/rendering/anari-device/camera/Perspective.cpp
@@ -23,7 +23,14 @@ void Perspective::commitParameters()
   Camera::commitParameters();
 
   this->m_fovy = this->getParam("fovy", viskores::Pi_3f());
-  this->m_aspect = this->getParam("aspect", viskores::Float32(1));
+  if (hasParam("aspect", ANARI_FLOAT32))
+  {
+    m_aspect = this->getParam<viskores::Float32>("aspect", 1.f);
+  }
+  else
+  {
+    m_aspect.reset();
+  }
   this->m_near = this->getParam("near", viskores::Float32(-1));
   this->m_far = this->getParam("far", viskores::Float32(-1));
 }
@@ -44,7 +51,12 @@ viskores::rendering::Camera Perspective::camera(const viskores::Bounds& bounds) 
   camera.SetLookAt(this->m_position + (length * this->m_direction));
   camera.SetViewUp(this->m_up);
   camera.SetFieldOfView(anari::degrees(this->m_fovy));
-  camera.SetAspectRatio(this->m_aspect);
+
+  if (this->m_aspect.has_value())
+  {
+    camera.SetAspectRatio(this->m_aspect.value());
+  }
+
   camera.SetClippingRange((this->m_near > 0) ? this->m_near : (0.01f * length),
                           (this->m_far > 0) ? this->m_far : (1000.f * length));
 
